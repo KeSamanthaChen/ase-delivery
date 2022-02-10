de622a29d3bb4c9f0f8a6d26e40981e444b2288e
Yanjie L <jasli1999@outlook.com>
2022-02-11 00:00:07 +0100

    update url

--- ase-delivery-spa/src/api/index.js
+++ ase-delivery-spa/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // export const api_url = 'http://localhost:10789/api';
-export const api_url = 'http://3.70.198.200:10789/api';
+export const api_url = 'http://3.71.93.212:10789/api';
 
 // TODO: change this to the real API instance
 const apiInstance = axios.create({
